556c126c3a82e0b4f4499804f83301421589a3c2
Mike Prechtl <prechtl.mi@gmail.com>
2019-10-14 17:18:01 +0200

    Add script to test functions

---
+++ functions/test_functions.sh
@@ -0,0 +1,12 @@
+
+blue=$'\e[1;34m'
+eblue=$'\e[0m'
+
+ABS_PATH=$(readlink -f $0)
+ABS_DIR=$(dirname $ABS_PATH)
+
+OPENFAAS_URL_FILE="${ABS_DIR}/../openfaas_ui_url.txt"
+OPENFAAS_URL=$(cat ${OPENFAAS_URL_FILE})
+
+printf "${blue}Test 'haveibeenpwned' function\n\n${eblue}"
+curl -X POST --url ${OPENFAAS_URL}/function/haveibeenpwned --data 'test@test.com'
