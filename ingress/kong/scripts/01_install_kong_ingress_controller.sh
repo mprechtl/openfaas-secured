a4dfdc7c04ceaa48bd92200106c537edf44e114b
Mike Prechtl <prechtl.mi@gmail.com>
2019-11-23 19:20:46 +0100

    Add database to Kong

--- ingress/kong/scripts/01_install_kong_ingress_controller.sh
+++ ingress/kong/scripts/01_install_kong_ingress_controller.sh
@@ -1,6 +1,8 @@
+ABS_PATH=$(readlink -f $0)
+ABS_DIR=$(dirname $ABS_PATH)
+
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm repo update
 
 kubectl create namespace kong
-helm install ingress stable/kong --namespace kong --set ingressController.enabled=true
-
+helm install ingress stable/kong --namespace kong -f ${ABS_DIR}/../config.yaml --set ingressController.enabled=true
