39ec314f58c60a3d391f5392f0787d5a6aca45ff
Mike Prechtl <prechtl.mi@gmail.com>
2019-11-24 16:32:32 +0100

    Remove password reference in config.yaml

--- ingress/kong/scripts/01_install_kong_ingress_controller.sh
+++ ingress/kong/scripts/01_install_kong_ingress_controller.sh
@@ -4,14 +4,9 @@ ABS_DIR=$(dirname $ABS_PATH)
 # Create kong namespace
 kubectl create namespace kong
 
-# generate a random password for database access
-PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
-
-# Create secret
-kubectl -n kong create secret generic kong \
-  --from-literal=postgres=${PASSWORD}
-
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm repo update
 
-helm install ingress stable/kong --namespace kong -f ${ABS_DIR}/../config.yaml --set ingressController.enabled=true --set env.database=postgres
+helm install ingress stable/kong --namespace kong -f ${ABS_DIR}/../config.yaml \
+  --set ingressController.enabled=true \
+  --set env.database=postgres
