66e05c364191ba1741a38d7570523b567ae60815
Mike Prechtl <prechtl.mi@gmail.com>
2019-11-24 17:43:46 +0100

    Wait til nginx is up

--- ingress/nginx/scripts/02_wait_til_nginx_is_up.sh
+++ ingress/nginx/scripts/02_wait_til_nginx_is_up.sh
@@ -1,2 +1,7 @@
-kubectl --namespace nginx get deployments
+blue=$'\e[1;34m'
+eblue=$'\e[0m'
+
+printf "${blue}Wait until Nginx ingress controller is up... (Then leave with CTRL + C)${eblue}\n\n"
+
+kubectl --namespace nginx get deployments --watch
 
